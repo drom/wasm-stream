328c020cffce9e5c0a8b63bf7db095917a820296
drom <alex.drom@gmail.com>
2017-03-30 23:17:38 -0700

    don't process .gitignore file

--- bin/wasm-all.js
+++ bin/wasm-all.js
@@ -12,7 +12,11 @@ fs.readdir(iPath, function (err, iFiles) {
     if (err) {
         throw err;
     }
-    iFiles.map(function (iFileName) {
+    iFiles
+    .filter(function (e) {
+        return e.match('^(.*).wasm$');
+    })
+    .map(function (iFileName) {
         var iStream = fs.createReadStream(path.resolve(iPath, iFileName));
         var oStream = fs.createWriteStream(path.resolve(oPath, iFileName + '.json'));
         var $ = wasmStream();
